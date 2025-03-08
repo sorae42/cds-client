547d747f80eb9e94f85b113d087f67e6312b1f31
Sora <34794115+sorae42@users.noreply.github.com>
2025-03-08 10:50:24 +0700

    feat: reimplement DataTable for crud operation

---
+++ src/lib/CRUDTable.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	const {
+		data,
+		presentation,
+		detailButton = true,
+		updateButton = true,
+		deleteButton = true
+	} = $props();
+</script>
+
+<div class="table-wrap">
+	<table class="table caption-bottom">
+		<thead>
+			<tr>
+				{#each presentation as text}
+					<th>{text}</th>
+				{/each}
+				<th class="!text-right">Thao tác</th>
+			</tr>
+		</thead>
+		<tbody class="[&>tr]:hover:preset-tonal-primary">
+			{#each data as sub}
+				<tr>
+					{#each Object.entries(sub) as [key, value], index (key)}
+						<td>{value}</td>
+					{/each}
+					<!-- Action buttons -->
+					<td class="!text-right">
+						{#if detailButton}
+							<a class="btn preset-filled" href="/details/{sub.id}">Chi tiết</a>
+						{/if}
+						{#if updateButton}
+							<button class="btn preset-filled-primary-500">Sửa</button>
+						{/if}
+						{#if deleteButton}
+							<button class="btn preset-filled-error-500">Xoá</button>
+						{/if}
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
---
+++ src/routes/manage/account/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import CrudTable from '$lib/CRUDTable.svelte';
+
+	const demo = [
+		{
+			id: '1',
+			username: 'amybibi',
+			email: 'amybibi@mail.com',
+			phone: '09169696969'
+		},
+		{
+			id: '2',
+			username: 'nhhai',
+			email: 'hai2k3@mail.com',
+			phone: '09169420303'
+		}
+	];
+</script>
+
+<div>
+	<CrudTable data={demo} presentation={['ID', 'Ten', 'Email', 'SDT']} />
+</div>
