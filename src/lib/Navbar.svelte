662476ab8058ecc29a44fbde41b1dfe600695219
Sora <34794115+sorae42@users.noreply.github.com>
2025-04-09 11:23:07 +0700

    style(layout): fixed navbar + extra links

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,17 +1,51 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import { Navigation } from '@skeletonlabs/skeleton-svelte';
-	import { Settings, User } from 'lucide-svelte';
+	import { ClipboardList, LandPlot, Star, User } from 'lucide-svelte';
+
+	let pathname = $state(page.url.pathname);
+
+	// place holder
+	let isAdmin = true;
 </script>
 
-<Navigation.Rail expanded={true}>
+<Navigation.Rail expanded={true} bind:value={pathname} classes="w-78 fixed z-[5]">
 	{#snippet tiles()}
-		<Navigation.Tile labelExpanded="Quản lý tài khoản" href="/management/account">
-			<User />
+		<Navigation.Tile labelExpanded="Danh sách tiêu chí" href="/criterias" id="/criterias">
+			<ClipboardList />
 		</Navigation.Tile>
-	{/snippet}
-	{#snippet footer()}
-		<Navigation.Tile labelExpanded="Cài đặt" href="/settings" title="Settings">
-			<Settings />
+
+		<Navigation.Tile labelExpanded="Đánh giá" href="#" id="#">
+			<Star />
 		</Navigation.Tile>
+
+		{#if isAdmin}
+			<hr class="bg-black w-full" />
+			<Navigation.Tile labelExpanded="Quản lý đơn vị" href="/management/area" id="/management/area">
+				<LandPlot />
+			</Navigation.Tile>
+
+			<Navigation.Tile
+				labelExpanded="Quản lý tài khoản"
+				href="/management/account"
+				id="/management/account"
+			>
+				<User />
+			</Navigation.Tile>
+			<Navigation.Tile
+				labelExpanded="Quản lý tiêu chí"
+				href="/management/criterias"
+				id="/management/criterias"
+			>
+				<ClipboardList />
+			</Navigation.Tile>
+			<Navigation.Tile
+				labelExpanded="Quản lý kỳ đánh giá"
+				href="/management/ratings"
+				id="/management/ratings"
+			>
+				<Star />
+			</Navigation.Tile>
+		{/if}
 	{/snippet}
 </Navigation.Rail>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,16 +10,22 @@
 
 	const routeThatHideNavbar = ['/auth', '/', '/search'];
 
-	let appbarTransparency = $state(true);
+	let appbarTransparency = $state(false);
 	let scrollY = $state(0);
 
+	$effect(() => {
+		if (page.url.pathname === '/') appbarTransparency = true;
+		else appbarTransparency = false;
+	});
+
 	function checkScroll() {
+		if (page.url.pathname !== '/') return;
 		if (scrollY < 42) appbarTransparency = true;
 		else appbarTransparency = false;
 	}
 </script>
 
-<AppBar classes="fixed {appbarTransparency ? 'bg-transparent' : ''} duration-300">
+<AppBar classes="fixed {appbarTransparency ? 'bg-transparent' : ''} duration-300 z-[10]">
 	{#snippet trail()}
 		{#if page.url.pathname !== '/search'}
 			<a href="/search" class="btn preset-outlined px-10">
@@ -44,8 +50,9 @@
 	{/snippet}
 </AppBar>
 
-<div class="border-surface-100-900 bg-white w-full h-full flex border-[1px]">
+<div class="border-surface-100-900 bg-white w-full h-full flex">
 	{#if !routeThatHideNavbar.includes(page.url.pathname)}
+		<div class="w-100"></div>
 		<Navbar />
 	{/if}
 	<div class="{page.url.pathname === '/' ? '' : 'pt-16'} !bg-white w-full">
