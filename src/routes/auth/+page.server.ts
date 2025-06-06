a024015b0325a3901dbf23effdc3fb2497049df6
Sora <34794115+sorae42@users.noreply.github.com>
2025-06-06 16:58:28 +0700

    enhancement(auth): remember device

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -7,13 +7,14 @@ export const actions = {
         var form = new URLSearchParams();
         form.append('username', data.get('username')?.toString() || '');
         form.append('password', data.get('password')?.toString() || '');
+        form.append('trustedToken', cookies.get('vn.CDS.trustfulMachine') || '');
 
-        const result = await submission({ 
-            method: 'POST', 
-            endpoint: '/auths/login', 
-            cookies, 
-            form, 
-            formType: 'url' 
+        const result = await submission({
+            method: 'POST',
+            endpoint: '/auths/login',
+            cookies,
+            form,
+            formType: 'url'
         });
 
         if (result.data.needOtp) {
@@ -45,6 +46,7 @@ export const actions = {
         var form = new URLSearchParams();
         form.append('username', data.get('username')?.toString() || '');
         form.append('otp', data.get('otp')?.toString() || '');
+        form.append('trustDevice', data.get('remember')?.toString() == "on" ? 'true' : 'false');
 
         const result = await submission({
             method: 'POST',
@@ -56,6 +58,7 @@ export const actions = {
 
         if (result.data.token) {
             cookies.set('vn.CDS.AuthToken', result.data.token, { path: '/' });
+            cookies.set('vn.CDS.trustfulMachine', (data.get('remember')?.toString() == "on" ? result.data.trustedToken : "Machine_of_the_Liar!"), { path: '/' });
         }
 
         if (!result.ok) {
